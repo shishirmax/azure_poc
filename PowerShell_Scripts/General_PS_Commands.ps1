0d497dc320003c87b90f175eec00e10fb2446948
Shishir <shishirmax@gmail.com>
2017-11-01 18:37:55 +0530

    new files added

--- PowerShell_Scripts/General_PS_Commands.ps1
+++ PowerShell_Scripts/General_PS_Commands.ps1
@@ -1,2 +1,7 @@
 #Check PowerShell Version
 $PSversionTable
+
+#Saving Data Directly to a Text File
+Get-Process | Out-File c:\scripts\test.txt
+
+#Saves all the running process in the system to the test.txt file.

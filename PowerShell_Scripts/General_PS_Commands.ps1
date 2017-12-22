a92762560af1e8de88bcec62949a689d0d6fb5fc
Shishir <shishirmax@gmail.com>
2017-12-22 11:47:23 +0530

    script updated

--- PowerShell_Scripts/General_PS_Commands.ps1
+++ PowerShell_Scripts/General_PS_Commands.ps1
@@ -2,6 +2,6 @@
 $PSversionTable
 
 #Saving Data Directly to a Text File
-Get-Process | Out-File c:\scripts\test.txt
+Get-Process | Out-File D:\GIT\Azure_poc\PowerShell_Scripts\File\ProcessList.txt
 
-#Saves all the running process in the system to the test.txt file.
+#Saves all the running process in the system to the ProcessList.txt file.

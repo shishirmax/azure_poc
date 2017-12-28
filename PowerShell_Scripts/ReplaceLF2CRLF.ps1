893faaca0bce0b51c132c7aba5be023b2228f219
Shishir <shishirmax@gmail.com>
2017-12-28 22:20:38 +0530

    script update

--- PowerShell_Scripts/ReplaceLF2CRLF.ps1
+++ PowerShell_Scripts/ReplaceLF2CRLF.ps1
@@ -1,6 +1,9 @@
 #Replace all LF EOF to CRLF
-param([string]$paramString)
-$path = $paramString
+#param([string]$paramString)
+#$path = $paramString
+
+$path = "D:\Edina\HomeSpotterFeed\27Dec17\edina_contata_sessions.csv"
+#$path = "D:\Edina\HomeSpotterFeed\25Dec17\HS_DataFeed_20171225.csv"
 #$path = "D:\GIT\Azure_poc\PowerShell_Scripts\File\edina_contata_sessions.csv"
 #$path = "D:\GIT\Azure_poc\PowerShell_Scripts\File\test.csv"
 $text = [IO.File]::ReadAllText($path) -replace "`n", "`r`n"
